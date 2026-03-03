7ed0bdf4297921f99bbf14349097b4e1e1ff06c9
Autoblogger <dev@famefact.de>
2026-03-03 22:30:00 +0100

    chore: update job listings from external sources (HR-JobScraper)
    25 jobs injected from: Arbeitsagentur, Indeed, StepStone, meinestadt.de

--- constants.ts
+++ constants.ts
@@ -3,63 +3,253 @@ import { Job, JobCategory } from './types';
 export const MOCK_JOBS: Job[] = [
   {
     id: '1',
-    title: 'Vorarbeiter Trockenbau (m/w/d)',
-    company: 'Müller Ausbau GmbH',
-    location: 'Berlin',
+    title: 'Trockenbauer (m/w/d)',
+    company: 'Wackler Personal-Service GmbH - NL Leipzig',
+    location: 'Zwenkau',
     type: 'Vollzeit',
-    category: JobCategory.TROCKENBAU,
-    salary: '55.000€ - 65.000€ / Jahr',
-    postedDate: '2023-10-15',
-    description: 'Wir suchen einen erfahrenen Vorarbeiter für Großprojekte im Berliner Raum. Sie koordinieren ein Team von 5-10 Mitarbeitern und überwachen die Qualität.',
-    requirements: ['Abgeschlossene Ausbildung', 'Meistertitel wünschenswert', '5 Jahre Berufserfahrung', 'Führerschein Klasse B']
+    category: JobCategory.OTHER,
+    description: 'Trockenbauer (m/w/d)',
+    postedDate: '2026-03-03'
   },
   {
     id: '2',
-    title: 'Maler & Lackierer für Sanierung',
-    company: 'Farbenfroh Wohnen',
-    location: 'München',
+    title: 'Tischler (m/w/d) im Innenausbau',
+    company: 'TimePartner Personalmanagement GmbH',
+    location: 'Haren (Ems)',
     type: 'Vollzeit',
-    category: JobCategory.MALER,
-    salary: '22€ / Stunde',
-    postedDate: '2023-10-18',
-    description: 'Für die Renovierung von Altbauwohnungen suchen wir Fachkräfte mit Auge fürs Detail. Spachteltechniken und hochwertige Lackierungen sind Ihr Spezialgebiet.',
-    requirements: ['Gesellenbrief', 'Selbstständige Arbeitsweise', 'Kundenfreundlichkeit']
+    category: JobCategory.OTHER,
+    description: 'Tischler (m/w/d) im Innenausbau',
+    postedDate: '2026-02-27'
   },
   {
     id: '3',
-    title: 'Bauleiter Innenausbau (m/w/d)',
-    company: 'ImmoConstruct AG',
-    location: 'Hamburg',
+    title: 'Ausbaufacharbeiter im Innenausbau (m/w/d)',
+    company: 'Käferstein Innenausbau GmbH',
+    location: 'Nürnberg, Mittelfranken',
     type: 'Vollzeit',
-    category: JobCategory.BAULEITUNG,
-    salary: '70.000€ - 85.000€ / Jahr',
-    postedDate: '2023-10-20',
-    description: 'Steuern Sie komplexe Ausbauprojekte von der Planung bis zur Übergabe. Sie sind der erste Ansprechpartner für Architekten und Subunternehmer.',
-    requirements: ['Studium Bauingenieurwesen oder Architektur', 'Erfahrung in VOB/C', 'Verhandlungsgeschick']
+    category: JobCategory.OTHER,
+    description: 'Ausbaufacharbeiter im Innenausbau (m/w/d)',
+    postedDate: '2026-02-26'
   },
   {
     id: '4',
-    title: 'Parkettleger / Bodenleger',
-    company: 'Hanseatic Floors',
-    location: 'Hamburg',
+    title: 'Schlosser Innenausbau (m/w/d)',
+    company: 'TimePartner Personalmanagement GmbH',
+    location: 'Salzgitter',
     type: 'Vollzeit',
-    category: JobCategory.BODEN,
-    salary: 'Nach Tarif + Zulagen',
-    postedDate: '2023-10-21',
-    description: 'Verlegen von hochwertigem Parkett und Designbelägen im gehobenen Privatkundenbereich.',
-    requirements: ['Ausbildung zum Bodenleger', 'Erfahrung mit Parkettschleifen', 'PKW-Führerschein']
+    category: JobCategory.OTHER,
+    description: 'Schlosser Innenausbau (m/w/d)',
+    postedDate: '2026-02-26'
   },
   {
     id: '5',
-    title: 'Trockenbauer für Akkordarbeiten',
-    company: 'SpeedBuild Systems',
-    location: 'Frankfurt am Main',
-    type: 'Projektbasis',
-    category: JobCategory.TROCKENBAU,
-    salary: 'Leistungsbezogen',
-    postedDate: '2023-10-22',
-    description: 'Für ein großes Bürogebäude suchen wir Subunternehmer oder Angestellte für reine Wand- und Deckenmontage.',
-    requirements: ['Hohe Leistungsbereitschaft', 'Eigenes Werkzeug von Vorteil', 'Teamfähigkeit']
+    title: 'Mitarbeiter Innenausbau (w/m/d)',
+    company: 'Sabine Jonuscheit & Frank Jonuscheit GbR',
+    location: 'Mühlhausen/Thüringen',
+    type: 'Vollzeit',
+    category: JobCategory.OTHER,
+    description: 'Mitarbeiter Innenausbau (w/m/d)',
+    postedDate: '2026-02-26'
+  },
+  {
+    id: '6',
+    title: 'Innenausbau Facharbeiter (m/w/d)',
+    company: 'Merseburger Baugesellschaft mbH',
+    location: 'Merseburg',
+    type: 'Vollzeit',
+    category: JobCategory.OTHER,
+    description: 'Innenausbau Facharbeiter (m/w/d)',
+    postedDate: '2026-02-26'
+  },
+  {
+    id: '7',
+    title: 'Mitarbeiter im Innenausbau (gn)',
+    company: 'Manpower GmbH & Co. KG',
+    location: 'Malchin',
+    type: 'Vollzeit',
+    category: JobCategory.OTHER,
+    description: 'Mitarbeiter im Innenausbau (gn)',
+    postedDate: '2026-02-25'
+  },
+  {
+    id: '8',
+    title: 'Tischler für den Innenausbau (m/w/d)',
+    company: 'Neo Temp GmbH',
+    location: 'Berlin',
+    type: 'Vollzeit',
+    category: JobCategory.OTHER,
+    description: 'Tischler für den Innenausbau (m/w/d)',
+    postedDate: '2026-02-23'
+  },
+  {
+    id: '9',
+    title: 'Mitarbeiter im Innenausbau (m/w/d) gesucht!!!',
+    company: 'Manuela Flenker Primus Jobinstitut',
+    location: 'Dresden',
+    type: 'Vollzeit',
+    category: JobCategory.OTHER,
+    description: 'Mitarbeiter im Innenausbau (m/w/d) gesucht!!!',
+    postedDate: '2026-02-23'
+  },
+  {
+    id: '10',
+    title: 'Bauleiter/in  - Innenausbau (Bauleiter/in)(m/w/d)',
+    company: 'PerZukunft Arbeitsvermittlung GmbH&Co.KG',
+    location: 'Berlin',
+    type: 'Vollzeit',
+    category: JobCategory.OTHER,
+    description: 'Bauleiter/in  - Innenausbau (Bauleiter/in)(m/w/d)',
+    postedDate: '2026-02-23'
+  },
+  {
+    id: '11',
+    title: 'Schreiner Innenausbau (m/w/d)',
+    company: 'persona service AG & Co. KG',
+    location: 'Wallersdorf, Niederbayern',
+    type: 'Vollzeit',
+    category: JobCategory.OTHER,
+    description: 'Schreiner Innenausbau (m/w/d)',
+    postedDate: '2026-02-16'
+  },
+  {
+    id: '12',
+    title: 'Trockenbaumonteur Schwerpunkt Innenausbau',
+    company: 'TM Ausbau GmbH Hauptverwaltung',
+    location: 'Chemnitz, Sachsen',
+    type: 'Vollzeit',
+    category: JobCategory.OTHER,
+    description: 'Trockenbaumonteur Schwerpunkt Innenausbau',
+    postedDate: '2026-02-12'
+  },
+  {
+    id: '13',
+    title: 'Holzmechaniker/in - Möbelbau und Innenausbau',
+    company: 'Einrichtungshaus Tischlerei Gitterle GmbH',
+    location: 'Fließ',
+    type: 'Vollzeit',
+    category: JobCategory.OTHER,
+    description: 'Holzmechaniker/in - Möbelbau und Innenausbau',
+    postedDate: '2026-02-12'
+  },
+  {
+    id: '14',
+    title: 'Holzmechaniker - Möbelbau und Innenausbau (m/w/d)',
+    company: 'Start NRW',
+    location: 'Warburg, Westfalen',
+    type: 'Vollzeit',
+    category: JobCategory.OTHER,
+    description: 'Holzmechaniker - Möbelbau und Innenausbau (m/w/d)',
+    postedDate: '2026-02-12'
+  },
+  {
+    id: '15',
+    title: 'Allrounder im Innenausbau (m/w/d)',
+    company: 'Dirk Stratmann',
+    location: 'Hamm, Westfalen',
+    type: 'Vollzeit',
+    category: JobCategory.OTHER,
+    description: 'Allrounder im Innenausbau (m/w/d)',
+    postedDate: '2026-02-11'
+  },
+  {
+    id: '16',
+    title: 'Monteur Innenausbau (m/w/d)',
+    company: 'ame GmbH NL Worms',
+    location: 'Bensheim',
+    type: 'Vollzeit',
+    category: JobCategory.OTHER,
+    description: 'Monteur Innenausbau (m/w/d)',
+    postedDate: '2026-02-06'
+  },
+  {
+    id: '17',
+    title: 'Monteur Innenausbau (m/w/d)',
+    company: 'ame GmbH NL Karlsruhe',
+    location: 'Rastatt',
+    type: 'Vollzeit',
+    category: JobCategory.OTHER,
+    description: 'Monteur Innenausbau (m/w/d)',
+    postedDate: '2026-02-06'
+  },
+  {
+    id: '18',
+    title: 'Holzmechaniker/in - Möbelbau und Innenausbau',
+    company: 'Maderböck Fenster u. Türen GmbH',
+    location: 'Maria Laah',
+    type: 'Vollzeit',
+    category: JobCategory.OTHER,
+    description: 'Holzmechaniker/in - Möbelbau und Innenausbau',
+    postedDate: '2026-01-27'
+  },
+  {
+    id: '19',
+    title: 'Holzmechaniker - Möbelbau und Innenausbau (m/w/d)',
+    company: 'confianza GmbH Erding',
+    location: 'München',
+    type: 'Vollzeit',
+    category: JobCategory.OTHER,
+    description: 'Holzmechaniker - Möbelbau und Innenausbau (m/w/d)',
+    postedDate: '2026-01-22'
+  },
+  {
+    id: '20',
+    title: 'Tischler (m/w/d) Innenausbau/Ladenbau',
+    company: 'Die Wohnwerkstatt Thomas Becker GmbH',
+    location: 'Blieskastel',
+    type: 'Vollzeit',
+    category: JobCategory.OTHER,
+    description: 'Tischler (m/w/d) Innenausbau/Ladenbau',
+    postedDate: '2026-01-20'
+  },
+  {
+    id: '21',
+    title: 'Innenausbau m/w/d',
+    company: 'Ruhlmann GmbH',
+    location: 'Groß-Zimmern',
+    type: 'Vollzeit',
+    category: JobCategory.OTHER,
+    description: 'Innenausbau m/w/d',
+    postedDate: '2025-11-04'
+  },
+  {
+    id: '22',
+    title: 'Servicemonteur (m/w/d) Innenausbau',
+    company: 'Terhalle Holding GmbH',
+    location: 'Ahaus',
+    type: 'Vollzeit',
+    category: JobCategory.OTHER,
+    description: 'Servicemonteur (m/w/d) Innenausbau',
+    postedDate: '2025-10-29'
+  },
+  {
+    id: '23',
+    title: 'Projektleiter Innenausbau (m/w/d)',
+    company: 'Schuk Consulting GmbH',
+    location: 'Mindelheim',
+    type: 'Vollzeit',
+    category: JobCategory.OTHER,
+    description: 'Projektleiter Innenausbau (m/w/d)',
+    postedDate: '2025-10-27'
+  },
+  {
+    id: '24',
+    title: 'Innenausbau mit Schwerpunkt auf Trockenbau',
+    company: 'Bogdan Padula',
+    location: 'Reckershausen, Hunsrück',
+    type: 'Vollzeit',
+    category: JobCategory.OTHER,
+    description: 'Innenausbau mit Schwerpunkt auf Trockenbau',
+    postedDate: '2025-09-25'
+  },
+  {
+    id: '25',
+    title: 'Innenausbau',
+    company: 'Reifschneider Bau GmbH',
+    location: 'Schwalbach am Taunus',
+    type: 'Vollzeit',
+    category: JobCategory.OTHER,
+    description: 'Innenausbau',
+    postedDate: '2025-09-18'
   }
 ];
 
